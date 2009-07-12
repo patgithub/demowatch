742ec764bb3ef03eeb9da25fcf6012eed1a25997
U-HAL9000\pat <pat@hal9000.(none)>
2009-07-12 17:33:43 +0200

    locale by browser language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,10 @@ class ApplicationController < ActionController::Base
   before_filter :redirect_to_www_demowatch_de
   before_filter :set_locale
   def set_locale
-    # if params[:locale] is nil then I18n.default_locale will be used
-    I18n.locale = params[:locale]
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
   end
-  # zur hauptdomain umleiten
   def redirect_to_www_demowatch_de
     if !request.host.include?('localhost') && request.host.match( /^www\.demowatch\.de/i).nil?
       redirect_to "http://www.demowatch.de" + request.path, :status=>:moved_permanently
@@ -42,6 +42,9 @@ protected
     flash[:notice] = t("layouts.application.flash.not_found")
     redirect_to :front
   end
-
+private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end  # zur hauptdomain umleiten
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
-  config.i18n.default_locale = :de
+  config.i18n.default_locale = :en
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
