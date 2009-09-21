275dfe6525cc11ba18e2f39a3c88a18cfe99c3f1
Patrick Hoffmann <pat@Cyano.(none)>
2009-09-21 23:18:55 +0200

    improved tweetlevel migration

--- db/migrate/20090921201646_add_event_tweetlevel.rb
+++ db/migrate/20090921201646_add_event_tweetlevel.rb
@@ -1,6 +1,9 @@
 class AddEventTweetlevel < ActiveRecord::Migration
   def self.up
     add_column :events, :tweetlevel, :integer
+    Event.all.each do |event|
+      event.update_attribute(:tweetlevel, [0,event.calc_tweetlevel-1].max)
+    end
   end
 
   def self.down
