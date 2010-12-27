10af455d0211cc193837a77e8af896e971567c9b
Simon Kröger <simon.kroeger@sponsorpay.com>
2010-12-27 18:44:00 +0100

    tweets via oauth

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,14 +1,16 @@
-class Tweet < ActiveResource::Base
+class Tweet
   # load twitter configuration
   TWITTER_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/twitter.yml")
 
-  self.site = TWITTER_CONFIG['site']
-  self.user = TWITTER_CONFIG['user']
-  self.password = TWITTER_CONFIG['password']
+  def self.config(key)
+    TWITTER_CONFIG[key.to_s]
+  end
 
   def self.update_status(content)
-    if !site.nil?
-      connection.post("/statuses/update.xml?status="+CGI.escape(content)) 
+    unless TWITTER_CONFIG.blank?
+      consumer = OAuth::Consumer.new(config(:consumer_key), config(:consumer_secret), :site => 'https://api.twitter.com', :scheme => :header)
+      access_token = OAuth::AccessToken.from_hash(consumer, :oauth_token => config(:oauth_token), :oauth_token_secret => config(:oauth_token_secret))
+      access_token.request(:post, 'http://api.twitter.com/statuses/update.xml?status=' + CGI.escape(content))
     end
   end
 end
